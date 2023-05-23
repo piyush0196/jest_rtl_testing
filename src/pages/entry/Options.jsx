a95918e1d87328a67c60522fb173acb093ef759a
piyush khodre <pkhodre@gammastack.com>
2023-05-23 10:10:01 +0530

    Test and code alert for server error response

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -2,18 +2,24 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { ScoopOption } from "./ScoopOption";
 import { ToppingOption } from "./ToppingOption";
+import { AlertBanner } from "../common/AlertBanner";
 
 export const Options = ({ optionType }) => {
   // optionType is 'scoops' or 'toppings'
 
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((res) => setItems(res.data))
-      .catch((err) => console.log(err));
+      .catch((err) => setError(true));
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
   const optionItems = items.map((item) => (
